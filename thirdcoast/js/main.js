c260386aac9b5d8257f8edf744928d72c4921313
hodgesh3 <hodgesh3@msu.edu>
2015-08-26 23:33:22 -0400

    home page/header updates

--- thirdcoast/js/main.js
+++ thirdcoast/js/main.js
@@ -1,4 +1,10 @@
 $('#nav-menu li a').on('click', function(){
     $('li a.current').removeClass('current');
+	$('li a').addClass('deselected');
+    $(this).addClass('current');
+});
+$('#nav-menu li').on('click', function(){
+    $('li.current').removeClass('current');
+	$('li').addClass('deselected');
     $(this).addClass('current');
 });
